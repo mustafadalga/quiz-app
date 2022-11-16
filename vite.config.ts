e4ca0dbe3e44ce541cabe0e4e8f738836fd17980
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-11-16 20:28:24 +0300

    Project has been initialized

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+
+  return {
+    plugins: [ vue() ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      strictPort: true,
+      port: 8080,
+      host: "::", //'0.0.0.0',
+    },
+    esbuild: {
+      drop: mode == "production" ? [ "console", "debugger" ] : []
+    }
+  };
+
+})

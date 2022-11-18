363db1002cb4c978088d0074b44f7d84fa84da8b
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-11-18 14:41:47 +0300

    ButtonNext component has been created

---
+++ src/components/ButtonNext.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="btn-primary btn-next">
+    <span>Next</span>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/variables/_index.scss";
+@import "@/scss/partials/_button.scss";
+
+.btn-next {
+  max-width: $br_2xs;
+  margin: 0 auto;
+  width: 100%;
+}
+</style>

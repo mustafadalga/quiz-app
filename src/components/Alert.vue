4df52392d7a2164a6a0125042d7697644fc081de
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-11-18 14:41:02 +0300

    Alert component has been created

---
+++ src/components/Alert.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import type { PropType } from "vue";
+import type { IWarning } from "@/types";
+
+const props = defineProps({
+  alert: {
+    type: Object as PropType<IWarning>,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div :class="['alert',`bg-${props.alert.color}`]">
+    {{ props.alert.message }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/local/_mixins.scss";
+@import "@/scss/variables/_colors.scss";
+
+.alert {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 6px 15px;
+  color: $colorWhite;
+  border-radius: 9999px;
+  font-size: 11px;
+  @include breakpoint("lg") {
+    font-size: 12px;
+  }
+  @include breakpoint("2xl") {
+    font-size: 13px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+  }
+}
+</style>

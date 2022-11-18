3dc25e3e199cf024c010bff67670c6dc2c1cbf85
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-11-18 14:33:15 +0300

    types has been created

---
+++ src/types/index.ts
@@ -0,0 +1,61 @@
+interface IQuestion {
+    question: string,
+    correct_answer: string,
+    incorrect_answers: string[],
+    options: string[]
+}
+
+interface IQuizResult {
+    correctAnswersNumber: number,
+    totalQuestionsNumber: number,
+    currentQuizSuccessRate: number,
+    allQuizzesSuccessRate: number,
+    hasAllQuizzesSuccessRate: boolean
+}
+
+interface IApiLoadState {
+    isLoaded: boolean,
+    isCalled: boolean
+}
+
+interface IWarning {
+    color: string,
+    message: string
+}
+
+
+interface IState {
+    quizzesResults: IQuizResult[],
+    questions: IQuestion[],
+    userAnswers: string[],
+    activeQuizResult: IQuizResult
+    categories: [],
+    numberOfQuestions: number,
+    activeQuestionNumber: number,
+    isQuizEnded: boolean | null,
+    activeComponent: string,
+    apiLoadState: {
+        [key: string]: IApiLoadState
+    },
+    warnings: IWarning[]
+}
+
+interface ICategory {
+    id: number,
+    name: string
+}
+
+interface IDifficulty {
+    id: string,
+    name: string
+}
+
+
+export type {
+    IQuestion,
+    IState,
+    ICategory,
+    IDifficulty,
+    IApiLoadState,
+    IWarning
+}

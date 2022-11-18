ccd71cfc639fa5a180297a7a1a065408db5261e6
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-11-18 18:23:02 +0300

    Colors has been improved

--- src/components/QuestionOption.vue
+++ src/components/QuestionOption.vue
@@ -40,6 +40,11 @@ function handleOption() {
   font-size: 10px;
   line-height: 16px;
 
+
+  &:disabled {
+    pointer-events: none;
+  }
+
   &:not(:disabled):hover {
     background-color: $colorGray200;
   }
@@ -48,7 +53,7 @@ function handleOption() {
     transform: scaleX(0.9);
   }
 
-  @include breakpoint("md"){
+  @include breakpoint("md") {
     font-size: 12px;
   }
   @include breakpoint("lg") {

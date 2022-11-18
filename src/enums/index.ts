6de2bea13d2507f8a35d417971e05cff1a042681
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-11-18 14:32:36 +0300

    ScreenComponents and ResponseCodes has been created

---
+++ src/enums/index.ts
@@ -0,0 +1,14 @@
+enum ScreenComponents {
+    Options = "Options",
+    Quiz = "Quiz",
+    Report = "Report"
+}
+
+enum ResponseCodes {
+    NoResult = 1
+}
+
+export {
+    ScreenComponents,
+    ResponseCodes
+}

e3baf100342069c697e6c589728f4c30bc34136f
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-11-18 14:42:54 +0300

    CustomSelect component has been created

---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+//@ts-ignore
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+import { ref, watch } from "vue";
+
+const props = defineProps({
+  options: {
+    type: Array,
+    default: () => []
+  },
+  selectedOption: {
+    type: Object,
+    default: () => ({})
+  },
+  placeholder: {
+    type: String,
+    default: "Type to search..."
+  },
+  clearable: {
+    type: Boolean,
+    default: true
+  }
+});
+
+const selectedItem = ref({});
+const watchOptions = {
+  immediate: true,
+}
+
+watch(() => props.selectedOption, (newValue) => selectedItem.value = newValue, watchOptions);
+</script>
+<template>
+  <v-select :options="props.options"
+            class="custom-select"
+            :clearable="props.clearable"
+            :placeholder="props.placeholder"
+            label="name"
+            :modelValue="selectedItem.name"
+            @update:modelValue="$emit('selected',$event ? $event : {})"/>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/local/_mixins.scss";
+
+:deep(.vs__dropdown-toggle) {
+  padding: 8px;
+}
+
+.custom-select :deep(*) {
+  font-size: 12px;
+  @include breakpoint("md") {
+    font-size: 15px;
+  }
+  @include breakpoint("2xl") {
+    font-size: 16px;
+  }
+}
+</style>
